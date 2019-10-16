dc6e68266431b9d0d92cc9a1532400520c940135
mahmoud.elsharkawy <mahmoud.elsharkawy@172.20.16.62>
2019-10-16 08:41:17 +0200

    Added constructors to the simple POM pages classes!

--- src/test/java/pom/simple/pages/GoogleHomePage.java
+++ src/test/java/pom/simple/pages/GoogleHomePage.java
@@ -4,20 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
-public class GoogleHomePage {	
-	private String url = "https://www.google.com/ncr";
-	private final static By google_search_bar = By.name("q");
-	
-	public void openURL(WebDriver driver) {
-		driver.get(url);
-	}
-	
-    public void googleSearch(WebDriver driver, String searchData){
-		driver.findElement(google_search_bar).sendKeys(searchData, Keys.ENTER);
+public class GoogleHomePage {
+    WebDriver driver;
+
+    private String url = "https://www.google.com/ncr";
+    private final static By google_search_bar = By.name("q");
+
+    public GoogleHomePage(WebDriver driver) {
+	this.driver = driver;
     }
-	
-    public String getTitleText(WebDriver driver) {
-    	return driver.getTitle();
+
+    public void openURL() {
+	driver.get(url);
+    }
+
+    public void googleSearch(String searchData) {
+	driver.findElement(google_search_bar).sendKeys(searchData, Keys.ENTER);
+    }
+
+    public String getTitleText() {
+	return driver.getTitle();
     }
 
 }
