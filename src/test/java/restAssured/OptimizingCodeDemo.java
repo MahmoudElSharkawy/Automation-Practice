dbb761165891102eddcc90994339bc3cfabcba52
sharkawy <mah.el-sharkawy@hotmail.com>
2019-10-30 22:21:06 +0200

    Added optimizing code demo to rest assured package

---
+++ src/test/java/restAssured/OptimizingCodeDemo.java
@@ -0,0 +1,74 @@
+package restAssured;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class OptimizingCodeDemo {
+    private static RequestSpecification requestSpec;
+    private static ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public static void createSpecifications() {
+
+        requestSpec = new RequestSpecBuilder().
+            setBaseUri("http://api.zippopotam.us").
+            build();
+        
+        responseSpec = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                build();
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills_withRequestSpec() {
+
+        given().
+            spec(requestSpec).
+        when().
+            get("us/90210").
+        then().
+            assertThat().
+            statusCode(200);
+    }
+
+    @Test
+    public void requestUsZipCode90210_checkPlaceNameInResponseBody_expectBeverlyHills_withResponseSpec() {
+
+        given().
+            spec(requestSpec).
+        when().
+            get("http://zippopotam.us/us/90210").
+        then().
+            spec(responseSpec).
+        and().
+            assertThat().
+            body("places[0].'place name'", equalTo("Beverly Hills"));
+    }
+
+    @Test
+    public void requestUsZipCode90210_extractPlaceNameFromResponseBody_assertEqualToBeverlyHills() {
+
+        String placeName =
+
+        given().
+            spec(requestSpec).
+        when().
+            get("http://zippopotam.us/us/90210").
+        then().
+            extract().
+            path("places[0].'place name'");
+
+        Assert.assertEquals("Beverly Hills", placeName);
+    }
+}
