7f9196003e85eb1e1cc83b3532068fbe057b5dfb
mahmoud.elsharkawy <mahmoud.elsharkawy@172.20.16.56>
2019-10-30 09:29:22 +0200

    Rest Assured Data Driven

--- src/test/java/assignments/PHPTravelsAssignment.java
+++ src/test/java/assignments/PHPTravelsAssignment.java
@@ -2,6 +2,9 @@ package assignments;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,68 +15,82 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import generic.BrowserFactory;
+
 public class PHPTravelsAssignment {
 
-	String firstName = "TestFN";
-	String lastName = "TestLN";
-	String email = "test@test.com";
-	String password = "12345678";
+    String firstName = "TestFN";
+    String lastName = "TestLN";
+    String email;
+    String password = "12345678";
 
-	WebDriver driver;
+    WebDriver driver;
+    Date date;
 
-	@BeforeClass
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/windows-64/chromedriver.exe");
+    @BeforeClass
+    public void setUp() {
+	date = new Date();
+	email = "test" + date.getTime() + "@test.com";
+	System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/mac-64/chromedriver");
+//	try {
+//	    BrowserFactory.browser("chrome", driver);
+//	} catch (IOException e) {
+//	    e.getMessage();
+//	}
 
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.phptravels.net/");
+    }
 
-		driver.get("https://www.phptravels.net/");
-	}
+    @Test(priority = 1)
+    public void Registration() {
 
-	@Test(priority = 1)
-	public void Registration() {
-		driver.findElement(By.linkText("MY ACCOUNT")).click();
-		driver.findElement(By.linkText("Sign Up")).click();
-		//tagname[@attribute='value']
-		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(firstName);
-		driver.findElement(By.name("lastname")).sendKeys(lastName);
-		driver.findElement(By.name("phone")).sendKeys("1523648987");
-		driver.findElement(By.name("email")).sendKeys(email);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.name("confirmpassword")).sendKeys(password);
-		//tagname[@attribute='value']
-		driver.findElement(By.xpath("//button[contains(@class,'signupbtn')]")).click();
-		
-		WebDriverWait wait = new WebDriverWait(driver, 20); 
-		wait.until(ExpectedConditions.titleContains("My Account"));
-		System.out.println(driver.getTitle());
-		
-		WebElement welcomeTxt = driver.findElement(By.xpath("//*[@style='margin-left: 17px']"));
+	driver.findElement(By.linkText("MY ACCOUNT")).click();
+	driver.findElement(By.linkText("Sign Up")).click();
+	// tagname[@attribute='value']
+	driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(firstName);
+	driver.findElement(By.name("lastname")).sendKeys(lastName);
+	driver.findElement(By.name("phone")).sendKeys("1523648987");
+	driver.findElement(By.name("email")).sendKeys(email);
+	driver.findElement(By.name("password")).sendKeys(password);
+	driver.findElement(By.name("confirmpassword")).sendKeys(password);
+	// tagname[@attribute='value']
+	driver.findElement(
+		By.xpath("//button[@type='submit' and contains (class, signupbtn) and contains(text() ,'Sign Up')]"))
+		.click();
 
-		assertEquals(welcomeTxt.getText(), "Hi, " + firstName + " " + lastName );
-	}
-	
-	@Test(priority = 2, dependsOnMethods = {"Registration"})
-	public void loginWithRegisteredData() {
-		driver.findElement(By.linkText(firstName.toUpperCase())).click();
-		driver.findElement(By.linkText("Logout")).click();
-		
-		driver.findElement(By.name("username")).sendKeys(email);
-		driver.findElement(By.name("password")).sendKeys(password);
-		driver.findElement(By.xpath("//button[contains(@class,'loginbtn')]")).click();
-		
-		WebDriverWait wait = new WebDriverWait(driver, 20); 
-		wait.until(ExpectedConditions.titleContains("My Account"));
-		System.out.println(driver.getTitle());
-		
-		WebElement welcomeTxt = driver.findElement(By.xpath("//*[@style='margin-left: 17px']"));
-		
-		assertEquals(welcomeTxt.getText(), "Hi, " + firstName + " " + lastName );
-	}
-	
-	@AfterClass
-	public void closeBrowser() {
-		driver.quit();
-	}
+	WebDriverWait wait = new WebDriverWait(driver, 20);
+	wait.until(ExpectedConditions.titleContains("My Account"));
+	System.out.println(driver.getTitle());
+
+	WebElement welcomeTxt = driver.findElement(By.xpath("//*[@style='margin-left: 17px']"));
+
+	assertEquals(welcomeTxt.getText(), "Hi, " + firstName + " " + lastName);
+    }
+
+    @Test(priority = 2, dependsOnMethods = { "Registration" })
+    public void loginWithRegisteredData() {
+	driver.findElement(By.linkText(firstName.toUpperCase())).click();
+	driver.findElement(By.linkText("Logout")).click();
+
+	driver.findElement(By.name("username")).sendKeys(email);
+	driver.findElement(By.name("password")).sendKeys(password);
+	driver.findElement(
+		By.xpath("//button[@type='submit' and contains (class, signupbtn) and contains(text() ,'Login')]"))
+		.click();
+
+	WebDriverWait wait = new WebDriverWait(driver, 20);
+	wait.until(ExpectedConditions.titleContains("My Account"));
+	System.out.println(driver.getTitle());
+
+	WebElement welcomeTxt = driver.findElement(By.xpath("//*[@style='margin-left: 17px']"));
+
+	assertEquals(welcomeTxt.getText(), "Hi, " + firstName + " " + lastName);
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+	driver.quit();
+    }
 }
--- src/test/java/generic/BrowserFactory.java
+++ src/test/java/generic/BrowserFactory.java
@@ -8,55 +8,53 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactory {
 
-	public static WebDriver browserDriver;
+    // Choose the Browser
+    public static void browser(String browserName, WebDriver driver) throws IOException {
 
-	// Choose the Browser
-	public static void browser(String browserName) throws IOException {
+	if (browserName == null) {
+	    throw new IOException("Browser name not found!!");
+	} else {
+	    if (browserName.equalsIgnoreCase("chrome") || browserName.equalsIgnoreCase("google chrome")) {
+		System.out.println("Opening Browser: " + browserName);
 
-		if (browserName == null) {
-			throw new IOException("Browser name not found!!");
+		if (System.getProperty("os.name").contains("Windows")) {
+		    System.setProperty("webdriver.chrome.driver",
+			    "src/main/resources/drivers/Windows-64/chromedriver.exe");
+		} else if (System.getProperty("os.name").contains("Mac")) {
+		    System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/Mac-64/chromedriver");
+		} else if (System.getProperty("os.name").contains("linux")) {
+		    System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/Linux-64/chromedriver");
 		} else {
-			if (browserName.equalsIgnoreCase("chrome") || browserName.equalsIgnoreCase("google chrome")) {
-				System.out.println("Opening Browser: " + browserName);
-
-				if (System.getProperty("os.name").contains("Windows")) {
-					System.setProperty("webdriver.chrome.driver",
-							"src/main/resources/drivers/Windows-64/chromedriver.exe");
-				} else if (System.getProperty("os.name").contains("Mac")) {
-					System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/Mac-64/chromedriver");
-				} else if (System.getProperty("os.name").contains("linux")) {
-					System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/Linux-64/chromedriver");
-				} else {
-					throw new IOException("Sorry, the selected OS is not valid or Not supported!!");
-				}
-
-				browserDriver = new ChromeDriver();
-				browserDriver.manage().window().maximize();
-
-			}
-
-			else if (browserName.equalsIgnoreCase("firefox") || browserName.equalsIgnoreCase("ff")) {
-				System.out.println("Opening Browser: " + browserName);
-
-				if (System.getProperty("os.name").contains("windows")) {
-					System.setProperty("webdriver.gecko.driver",
-							"src/main/resources/drivers/Windows-64/geckodriver.exe");
-				} else if (System.getProperty("os.name").contains("mac")) {
-					System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/Mac-64/geckodriver.exe");
-				} else if (System.getProperty("os.name").contains("linux")) {
-					System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/Linux-64/geckodriver");
-				} else {
-					throw new IOException("The selected OS is not valid or Not supported!!");
-				}
-
-				browserDriver = new FirefoxDriver();
-				browserDriver.manage().window().maximize();
-
-			} else {
-				throw new IOException("The entered browser name is not valid or Not supported!!");
-			}
+		    throw new IOException("Sorry, the selected OS is not valid or Not supported!!");
 		}
 
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+	    }
+
+	    else if (browserName.equalsIgnoreCase("firefox") || browserName.equalsIgnoreCase("ff")) {
+		System.out.println("Opening Browser: " + browserName);
+
+		if (System.getProperty("os.name").contains("windows")) {
+		    System.setProperty("webdriver.gecko.driver",
+			    "src/main/resources/drivers/Windows-64/geckodriver.exe");
+		} else if (System.getProperty("os.name").contains("mac")) {
+		    System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/Mac-64/geckodriver.exe");
+		} else if (System.getProperty("os.name").contains("linux")) {
+		    System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/Linux-64/geckodriver");
+		} else {
+		    throw new IOException("The selected OS is not valid or Not supported!!");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+
+	    } else {
+		throw new IOException("The entered browser name is not valid or Not supported!!");
+	    }
 	}
+
+    }
 
 }
