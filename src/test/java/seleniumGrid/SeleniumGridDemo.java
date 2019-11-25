6cd41f1a95691be15bce45c78032e109fbdd559d
mahmoud.elsharkawy <mahmoud.elsharkawy@172.20.16.51>
2019-11-25 14:59:32 +0200

    Finished the Selenium Grid Demo with Docker containers

--- src/test/java/seleniumGrid/SeleniumGridDemo.java
+++ src/test/java/seleniumGrid/SeleniumGridDemo.java
@@ -1,42 +1,60 @@
 package seleniumGrid;
 
-import java.net.MalformedURLException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
 import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
+
+/*
+ * Steps to execute the grid with Docker:
+ * 
+ * After installing Docker successfully:
+ * docker version
+ * docker pull selenium/hub
+ * docker images (to see the images installed)
+ * docker pull selenium/node-chrome
+ * docker pull selenium/node-chrome-debug
+ * docker pull selenium/node-firefox
+ * docker pull selenium/node-firefox-debug
+ * 
+ * Create the containers and connect them together in one command using docker compose:
+ * put the .yml file into the project and cd to it from the terminal
+ * docker-compose up -d
+ * docker ps -a (to see what we just ran)
+ * http://localhost:4444/grid/console
+ * 
+ * 
+ * Excecute from the grid.xml suite
+ */
 
 public class SeleniumGridDemo {
 	RemoteWebDriver driver;
 	public static final String host = "localhost";
 	public static final String port = "4444";
  
+	@Parameters("browserName")
 	@BeforeClass
-	public void setup() {
+	public void setup(String browserName) {
 		driver = null;
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("browserName", "chrome");
-		capabilities.setCapability("version", "78.0");
-		capabilities.setCapability("platform", "win10"); // If this cap isn't specified, it will just get the any
-		capabilities.setCapability("network", true); // To enable network logs
-		capabilities.setCapability("visual", true); // To enable step by step screenshot
-		capabilities.setCapability("video", true); // To enable video recording
-		capabilities.setCapability("console", true); // To capture console logs
+		capabilities.setCapability("browserName", browserName);
+		capabilities.setCapability("version", "ANY");
+//		capabilities.setCapability("platform", "win10"); // If this cap isn't specified, it will just get the any
+//		capabilities.setCapability("network", true); // To enable network logs
+//		capabilities.setCapability("visual", true); // To enable step by step screenshot
+//		capabilities.setCapability("video", true); // To enable video recording
+//		capabilities.setCapability("console", true); // To capture console logs
 		try {
-			driver = new RemoteWebDriver(new URL("https://" + host + ":" + port + "/wb/hub"), capabilities);
+			driver = new RemoteWebDriver(new URL("http://" + host + ":" + port + "/wd/hub"), capabilities);
 		} catch (Exception e) {
 			System.out.println("Invalid grid URL" + e.getMessage());
 		}
-
 	}
 
 	@Test
