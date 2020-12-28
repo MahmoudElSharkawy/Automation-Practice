94eb44e2ce0420141ac8821e7863381cb7137767
Mahmoud El-Sharkawy <Mahmoud El-Sharkawy@10.70.118.207>
2020-12-28 14:32:05 +0200

    Enhancements

--- src/main/java/utils/ElementActions.java
+++ src/main/java/utils/ElementActions.java
@@ -3,7 +3,6 @@ package utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import io.qameta.allure.Step;
