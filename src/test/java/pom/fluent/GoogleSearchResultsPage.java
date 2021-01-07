0c94a4a572dc59dbf14b8865ccc764061c10e232
El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2021-01-07 02:34:43 +0200

    Enhance pom.fluent example

--- src/test/java/pom/fluent/GoogleSearchResultsPage.java
+++ src/test/java/pom/fluent/GoogleSearchResultsPage.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.WebDriver;
 public class GoogleSearchResultsPage {
     private WebDriver driver;
 
-    private By google_search_result = By.xpath("(//h3[contains(@class,'LC20lb')])[1]");
+//    private By google_search_result = By.xpath("(//h3[contains(@class,'LC20lb')])[1]");
+    private By google_search_result(String resultIndex) {
+	return By.xpath("(//h3[contains (@class, 'LC20lb')])" + "[" + resultIndex + "]");
+    }
 
     public GoogleSearchResultsPage(WebDriver driver) {
 	this.driver = driver;
@@ -16,12 +19,8 @@ public class GoogleSearchResultsPage {
 	return driver.getTitle();
     }
 
-    public String getSearchResultText() {
-	return driver.findElement(google_search_result).getText();
-    }
-
-    public void clickOnSearchResult() {
-	driver.findElement(google_search_result).click();
+    public String getSearchResultText(String resultIndex) {
+	return driver.findElement(google_search_result(resultIndex)).getText();
     }
 
 }
--- src/test/java/pom/fluent/GoogleSearchTest.java
+++ src/test/java/pom/fluent/GoogleSearchTest.java
@@ -2,42 +2,37 @@ package pom.fluent;
 
 import static org.testng.Assert.assertEquals;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import utils.BrowserFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.SeleniumEventReporter;
 
 public class GoogleSearchTest {
-    private WebDriver driver;
+    EventFiringWebDriver driver;
 
     @BeforeClass
     public void setup() {
-	driver = BrowserFactory.openBrowser();
-    }
-
-    @BeforeMethod
-    public void beforemethod() {
-	driver = new ChromeDriver();
+	WebDriverManager.chromedriver().setup();
+	driver = new EventFiringWebDriver(new ChromeDriver());
+	driver.register(new SeleniumEventReporter());
+	driver.manage().window().maximize();
 	new GoogleHomePage(driver).openURL();
+
     }
 
     @Test
     public void TestGoogleSearch() {
-	assertEquals(new GoogleHomePage(driver).googleSearch("Selenium WebDriver").getSearchResultText(),
-		"What is Selenium WebDriver? Difference with RC - Guru99");
+	assertEquals(new GoogleHomePage(driver)
+		.googleSearch("Selenium WebDriver")
+		.getSearchResultText("1"),
+		"WebDriver :: Documentation for Selenium");
     }
 
-    @Test
-    public void TestGoogleSearchAndClickOnTheFirstResult() {
-	new GoogleHomePage(driver).googleSearch("Selenium WebDriver").clickOnSearchResult();
-
-    }
-
-    @AfterMethod
+    @AfterClass
     public void quitWebDriver() {
 	driver.quit();
     }
