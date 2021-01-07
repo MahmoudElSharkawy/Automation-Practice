121e8d7734650f595ce5530461b2645f2ed2d46f
El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2021-01-07 17:26:30 +0200

    Enhancements

--- src/main/java/utils/BrowserFactory.java
+++ src/main/java/utils/BrowserFactory.java
@@ -3,7 +3,6 @@ package utils;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,8 +18,8 @@ public class BrowserFactory {
     private static String browserTypeProperty = PropertiesReader.getProperty("liveproject.properties", "browser.type");
     private static String executionTypeProperty = PropertiesReader.getProperty("liveproject.properties",
 	    "execution.type");
-    private static String host = "localhost";
-    private static String port = "4444";
+    private static String host = PropertiesReader.getProperty("liveproject.properties", "remote.execution.host");
+    private static String port = PropertiesReader.getProperty("liveproject.properties", "remote.execution.port");
 
     public enum BrowserType {
 	MOZILLA_FIREFOX("Mozilla Firefox"), GOOGLE_CHROME("Google Chrome"), FROM_PROPERTIES(browserTypeProperty);
@@ -50,16 +49,15 @@ public class BrowserFactory {
 	if (executionType == ExecutionType.REMOTE || (executionType == ExecutionType.FROM_PROPERTIES
 		&& executionTypeProperty.equalsIgnoreCase("remote"))) {
 	    /*
-	     * Steps to executo remotely with selenium grid and dockers VERY simple
-	     * steps:... 1- Install docker 2- You need to have a .yml file to configure the
-	     * network between the containers like that we have in the project root file
-	     * "docker-compose_native.yml" 3- open a terminal on the project directory 4-
-	     * Enter the following command that will setup the containers (1 hub & 4 nodes)
-	     * and run them automatically: docker-compose -f docker-compose_native.yml up
-	     * --scale chrome=4 --remove-orphans -d 5- Enter the following command to check
-	     * the running containers: docker ps 6- open a browser and enter this url to see
-	     * the grid :D http://localhost:4444/ui/index.html 7- execute using this
-	     * condition
+	     * Steps to executo remotely with selenium grid and dockers VERY simpl steps:... 
+	     * 1- Install docker 
+	     * 2- You need to have a .yml file to configure the network between the containers like that we have in the src/main/resource file "docker-compose_native.yml" 
+	     * 3- open a terminal on the project directory 
+	     * 4- Enter the following command that will setup the containers (1 hub & 4 nodes) and run them automatically: 
+	     * docker-compose -f docker-compose_native.yml up --scale chrome=4 --remove-orphans -d 
+	     * 5- Enter the following command to check the running containers: docker ps 
+	     * 6- open a browser and enter this url to see the grid :D http://localhost:4444/ui/index.html 
+	     * 7- execute using this condition
 	     */
 	    if (browserType == BrowserType.GOOGLE_CHROME
 		    || (browserType == BrowserType.FROM_PROPERTIES && browserTypeProperty.equalsIgnoreCase("chrome"))) {
@@ -142,36 +140,34 @@ public class BrowserFactory {
     ////////////////////////////////////////////////////////////////////////
     private static ChromeOptions getChromeOptions_remote() {
 	ChromeOptions chOptions = new ChromeOptions();
+	chOptions.addArguments("--window-size=1920,1080");
+	chOptions.setHeadless(true);
+
 //	chOptions.setCapability("platform", Platform.LINUX);
 //	chOptions.addArguments("--headless");
 //	chOptions.addArguments("--start-maximized");
-	chOptions.addArguments("--window-size=1920,1080");
 //	chOptions.addArguments("disable--infobars");
 //	chOptions.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
-	chOptions.setHeadless(true);
 	return chOptions;
     }
 
     private static FirefoxOptions getFirefoxOptions_remote() {
 	FirefoxOptions ffOptions = new FirefoxOptions();
-	ffOptions.setCapability("platform", Platform.LINUX);
-	ffOptions.addArguments("--headless");
-//	ffOptions.addArguments("--start-maximized");
 	ffOptions.addArguments("--window-size=1920,1080");
-//	ffOptions.addArguments("disable--infobars");
+	ffOptions.setHeadless(true);
 	return ffOptions;
     }
     
     private static ChromeOptions getChromeOptions_localHeadless() {
 	ChromeOptions chOptions = new ChromeOptions();
-	chOptions.addArguments("--headless");
+	chOptions.setHeadless(true);
 	chOptions.addArguments("--window-size=1920,1080");
 	return chOptions;
     }
 
     private static FirefoxOptions getFirefoxOptions_localHeadless() {
 	FirefoxOptions ffOptions = new FirefoxOptions();
-	ffOptions.addArguments("--headless");
+	ffOptions.setHeadless(true);
 	ffOptions.addArguments("--window-size=1920,1080");
 	return ffOptions;
     }
