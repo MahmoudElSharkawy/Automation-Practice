0288051f26ed1e95094c9a2244d8078bf0a96932
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2021-02-07 23:32:59 +0200

    Enhancements to TestNG listeners

--- src/main/java/utils/SuiteListener.java
+++ src/main/java/utils/SuiteListener.java
@@ -8,14 +8,14 @@ public class SuiteListener implements ISuiteListener {
     @Override
     public void onStart(ISuite suite) {
 	System.out.println("\n" + "*******************************************");
-	System.out.println("Starting Test Suite; By Mahmoud El-SHarkawy!");
+	System.out.println("Starting Test Suite; By Mahmoud El-Sharkawy!");
 	System.out.println("*******************************************" + "\n");
     }
 
     @Override
     public void onFinish(ISuite suite) {
 	System.out.println("\n" + "*******************************************");
-	System.out.println("Finished Test Suite; By Mahmoud El-SHarkawy!");
+	System.out.println("Finished Test Suite; By Mahmoud El-Sharkawy!");
 	System.out.println("*******************************************" + "\n");
     }
 }
