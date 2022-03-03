d494c57457f2f87dc5d2b2d6e2109a79ed783525
mahmoud.elsharkawy <mahmoud.elsharkawy@gizasystems.com>
2022-03-03 10:11:53 +0200

    Add Shadow Dom Demo test class

---
+++ src/test/java/examples/selenium/ShadowDomDemo.java
@@ -0,0 +1,30 @@
+package examples.selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ShadowDomDemo {
+    @Test
+    public void shadowDomTest() {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	
+        driver.get("http://watir.com/examples/shadow_dom.html");
+        WebElement shadowHost = driver.findElement(By.cssSelector("#shadow_host"));
+        SearchContext shadowRoot = shadowHost.getShadowRoot();
+        WebElement shadowContent = shadowRoot.findElement(By.cssSelector("#shadow_content"));
+        Assert.assertEquals(shadowContent.getText(), "some text");
+        
+        driver.quit();
+    }
+}
