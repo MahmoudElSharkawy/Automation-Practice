1974ec38541e764a6b411476befad9fae57afa41
mahmoud.elsharkawy <mahmoud.elsharkawy@gizasystems.com>
2022-03-06 20:16:30 +0200

    Add GeoLocationDemo test class

---
+++ src/test/java/examples/selenium/GeoLocationDemo.java
@@ -0,0 +1,49 @@
+package examples.selenium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v96.emulation.Emulation;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GeoLocationDemo {
+    ChromeDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked", "serial" })
+    @Test
+    public void mockGeoLocation_executeCDPCommand() {
+	Map coordinates = new HashMap() {
+	    {
+		put("latitude", 32.746940);
+		put("longitude", -97.092400);
+		put("accuracy", 1);
+	    }
+	};
+	driver.executeCdpCommand("Emulation.setGeolocationOverride", coordinates);
+	driver.get("https://where-am-i.org/");
+    }
+
+    @Test
+    public void mockGeoLocation_DevTools() {
+	DevTools devTools = driver.getDevTools();
+	devTools.createSession();
+	devTools.send(Emulation.setGeolocationOverride(
+		Optional.of(52.5043), 
+		Optional.of(13.4501), 
+		Optional.of(1)));
+	driver.get("https://my-location.org/");
+    }
+}
