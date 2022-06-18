4c1c1243e0edab46c9d862963955499294253617
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2022-06-18 15:42:19 +0200

    Add Json File Manager class

--- src/main/java/utils/JsonFileManager.java
+++ src/main/java/utils/JsonFileManager.java
@@ -1,5 +1,34 @@
 package utils;
 
+import static org.testng.Assert.fail;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import io.restassured.path.json.JsonPath;
+
 public class JsonFileManager {
-    // TODO: This class should manage getting test data from json files
+    private final String jsonFilePath;
+    private FileReader reader;
+
+    public JsonFileManager(String jsonFilePath) {
+	this.jsonFilePath = jsonFilePath;
+	try {
+	    reader = new FileReader(jsonFilePath);
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	    fail(e.getMessage());
+	}
+    }
+
+    public String getTestData(String jsonPath) {
+	Object testData = null;
+	try {
+	    reader = new FileReader(jsonFilePath);
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+	testData = JsonPath.from(reader).getString(jsonPath);
+	return String.valueOf(testData);
+    }
 }
