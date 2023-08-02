8027990046550a589fd2ee5a938bb2b4a9135157
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2023-08-02 21:17:43 +0200

    Enhance JsonFileManager file

--- src/main/java/utils/JsonFileManager.java
+++ src/main/java/utils/JsonFileManager.java
@@ -1,34 +1,41 @@
 package utils;
 
-import static org.testng.Assert.fail;
 
-import java.io.FileNotFoundException;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.PathNotFoundException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+
 import java.io.FileReader;
+import java.io.IOException;
 
-import io.restassured.path.json.JsonPath;
 
 public class JsonFileManager {
-    private final String jsonFilePath;
-    private FileReader reader;
+    String jsonReader;
+    String jsonFilePath;
 
     public JsonFileManager(String jsonFilePath) {
-	this.jsonFilePath = jsonFilePath;
-	try {
-	    reader = new FileReader(jsonFilePath);
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	    fail(e.getMessage());
-	}
+        this.jsonFilePath = jsonFilePath;
+        try {
+            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(jsonFilePath));
+            jsonReader = data.toJSONString();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
     }
 
     public String getTestData(String jsonPath) {
-	Object testData = null;
-	try {
-	    reader = new FileReader(jsonFilePath);
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	}
-	testData = JsonPath.from(reader).getString(jsonPath);
-	return String.valueOf(testData);
+        String testData = "";
+        try {
+            testData = JsonPath.read(jsonReader, jsonPath);
+        } catch (PathNotFoundException e) {
+            e.printStackTrace();
+            Assert.fail("No results for json path: '" + jsonPath + "' in the json file: '" + this.jsonFilePath + "'");
+        }
+        return testData;
     }
+
 }
